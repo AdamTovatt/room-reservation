60ce9daf8428c0fb1ffc94a677cb2bec497eb6d7
Adam Tovatt <sporedenstore@hotmail.com>
2022-06-23 21:41:46 +0200

    add button to information header

--- src/Components/BuildingInformationHeader.js
+++ src/Components/BuildingInformationHeader.js
@@ -3,26 +3,41 @@ import { Room } from "./Room";
 import { Building } from "./Building";
 import { GetDayNameByOffset } from "./DayOffsetPicker";
 
-export const BuildingInformationHeader = ({ dayOffset }) => {
+export const BuildingInformationHeader = ({ dayOffset, setInformationHeaderVisible }) => {
   return (
-    <InformationHeader>
-      Reservationer {GetDayNameByOffset(dayOffset, true).toLocaleLowerCase()}{" "}
-      {dayOffset === 0
-        ? "klockan " +
+    <HeaderContainer>
+      <InformationHeader>
+        Reservationer {GetDayNameByOffset(dayOffset, true).toLocaleLowerCase()}{" "}
+        {dayOffset === 0
+          ? "klockan " +
           new Date()
             .toLocaleTimeString("en-US", { hour12: false })
             .replace(/(.*)\D\d+/, "$1")
-        : ""}
-    </InformationHeader>
+          : ""}
+      </InformationHeader>
+      <SetVisibleButton></SetVisibleButton>
+    </HeaderContainer>
   );
 };
 
-const InformationHeader = styled.div`
+const SetVisibleButton = styled.div`
+  width: 1em;
+  height: 1em;
+  background-color: red;
+  margin-top: 0.2em;
+`
+
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: row;
   margin-top: 1.8em;
+  margin-left: 0.4em;
+`
+
+const InformationHeader = styled.div`
   padding: 0.1em;
   max-width: 30em;
   overflow: hidden;
   text-align: left;
-  margin-left: 0.4em;
   font-weight: 600;
 `;
--- src/Components/RoomInformationModal.js
+++ src/Components/RoomInformationModal.js
@@ -32,7 +32,6 @@ export const RoomInformationModal = ({ room, setRoomModal }) => {
       : ""
     : "";
 
-  console.log(currentReservation);
   return (
     <ModalBackground onClick={() => setRoomModal(null)}>
       <ModalBackplate>
