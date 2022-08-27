c7a5b6bbb40c4c1ef1f9fd893e3ef7f7f0705bd6
Adam Tovatt <sporedenstore@hotmail.com>
2022-08-27 13:44:14 +0200

    remove second loading screen text

--- src/Components/LoadingScreen.js
+++ src/Components/LoadingScreen.js
@@ -6,9 +6,6 @@ export const LoadingScreen = () => {
     <LoadingContainer>
       <LoadingSpin primaryColor="#456990" secondaryColor="#49BEAA" />
       <LoadingText>Hämtar salschema...</LoadingText>
-      <LoadingText>
-        Det kan ta några sekunder om det är första gången på länge
-      </LoadingText>
     </LoadingContainer>
   );
 };
